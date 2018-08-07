d0ce6e049a41ede40c535ad5fe61d85f59049685
Gabriel Peal <gabriel.peal@airbnb.com>
2018-08-07 13:55:15 -0700

    Created an empty sample app

---
+++ sample/src/main/java/com/airbnb/sample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.airbnb.sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
