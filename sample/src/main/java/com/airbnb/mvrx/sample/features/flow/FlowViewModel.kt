403aabfdad66a2758bf0ca5c9987ad5ad6da9e2e
Gabriel Peal <gabriel.peal@airbnb.com>
2018-08-09 11:38:38 -0700

    Restore state if the map is empty

--- sample/src/main/java/com/airbnb/mvrx/sample/features/flow/FlowViewModel.kt
+++ sample/src/main/java/com/airbnb/mvrx/sample/features/flow/FlowViewModel.kt
@@ -1,13 +1,16 @@
 package com.airbnb.mvrx.sample.features.flow
 
 import com.airbnb.mvrx.MvRxState
+import com.airbnb.mvrx.PersistState
 import com.airbnb.mvrx.sample.core.MvRxViewModel
 
-data class FlowState(val count: Int = 0) : MvRxState
+/**
+ * [PersistState] will persist the count if Android kills the process in the background
+ * and restores it in a new process.
+ */
+data class FlowState(@PersistState val count: Int = 0) : MvRxState
 
 class FlowViewModel(override val initialState: FlowState) : MvRxViewModel<FlowState>() {
 
     fun setCount(count: Int) = setState { copy(count = count) }
-
-    fun incrementCount() = setState { copy(count = count + 1) }
 }
