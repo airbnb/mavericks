1f4a2ade1afa7b5a5239d9f817cb19264d72e0de
Gabriel Peal <gabriel.peal@airbnb.com>
2018-08-09 22:51:08 -0700

    Switch emulator path

--- start_emulator.sh
+++ start_emulator.sh
@@ -1,3 +1,3 @@
 echo no | avdmanager create avd -n travis -k "system-images;android-28;default;x86_64" --device "Nexus 5" --force
-emulator -avd travis -no-audio -no-window &
+$ANDROID_HOME/emulator/emulator -avd travis -no-audio -no-window &
 adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done; input keyevent 82'
