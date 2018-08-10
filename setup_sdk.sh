2f90d28aa147fc6c47a9fa1f65ed98800c4cc89a
Gabriel Peal <gabriel.peal@airbnb.com>
2018-08-09 22:05:07 -0700

    touch repositories.cfg

--- setup_sdk.sh
+++ setup_sdk.sh
@@ -9,5 +9,6 @@ mkdir -p $HOME/.cache
 curl https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip --output $HOME/.cache/sdk.zip
 unzip -q $HOME/.cache/sdk.zip -d $ANDROID_HOME
 
+touch $HOME/.android/repositories.cfg
 sdkmanager --update
 sdkmanager "platform-tools" "platforms;android-27" "system-images;android-27;default;x86_64"
