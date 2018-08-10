e2a1e2cecc902869e0cf0cf6aa5f185ef5150e9b
Gabriel Peal <gabriel.peal@airbnb.com>
2018-08-09 22:03:22 -0700

    silent -> quiet

--- setup_sdk.sh
+++ setup_sdk.sh
@@ -7,7 +7,7 @@ fi
 
 mkdir -p $HOME/.cache
 curl https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip --output $HOME/.cache/sdk.zip
-unzip -s $HOME/.cache/sdk.zip -d $ANDROID_HOME
+unzip -q $HOME/.cache/sdk.zip -d $ANDROID_HOME
 
 sdkmanager --update
 sdkmanager "platform-tools" "platforms;android-27" "system-images;android-27;default;x86_64"
