75ed67cadd4ad7042b1446eef7b64f9d88d18313
Gabriel Peal <gabriel.peal@airbnb.com>
2018-08-09 23:17:39 -0700

    Fix image component name

--- start_emulator.sh
+++ start_emulator.sh
@@ -2,7 +2,7 @@
 set -e
 set -o pipefail
 
-$ANDROID_HOMEtools/binsdkmanager "system-images;android-24;default;armeabi-v7a"
+# $ANDROID_HOMEtools/binsdkmanager "system-images;android-24;default;armeabi-v7a"
 echo no | avdmanager create avd -n travis -k "system-images;android-24;default;armeabi-v7a" --device "Nexus 5" --force --abi "armeabi-v7a"
 $ANDROID_HOMEemulator/emulator -avd travis -no-audio -no-window &
 adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done; input keyevent 82'
