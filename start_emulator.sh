b5b74a0ca2278b72c169053c9d26d0aeb95a866a
Gabriel Peal <gabriel.peal@airbnb.com>
2018-08-10 00:23:26 -0700

    Renamed image and added logs

--- start_emulator.sh
+++ start_emulator.sh
@@ -10,7 +10,10 @@ set -o pipefail
 android list target
 echo y | android update sdk -a --no-ui --filter android-24
 echo y | android update sdk -a --no-ui --filter sys-img-armeabi-v7a-android-24
-echo no | android create avd --force -n test -t android-24 --abi armeabi-v7a
-emulator -avd test -no-audio -no-window &
+echo no | android create avd --force -n travis -t android-24 --abi armeabi-v7a
+echo "-----Starting emulator"
+emulator -avd travis -no-audio -no-window &
+echo "-----Waiting for boot"
 android-wait-for-emulator
 adb shell input keyevent 82 &
+echo "-----Booted"
