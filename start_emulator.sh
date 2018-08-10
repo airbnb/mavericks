fae22419124c1cbe444b6c06b4922c39a2e8fa31
Gabriel Peal <gabriel.peal@airbnb.com>
2018-08-09 23:37:23 -0700

    Accept license

--- start_emulator.sh
+++ start_emulator.sh
@@ -2,7 +2,7 @@
 set -e
 set -o pipefail
 
-/usr/local/android-sdk/tools/bin/sdkmanager "system-images;android-24;default;armeabi-v7a"
+yes | /usr/local/android-sdk/tools/bin/sdkmanager "system-images;android-24;default;armeabi-v7a"
 echo no | avdmanager create avd -n travis -k "system-images;android-24;default;armeabi-v7a" --device "Nexus 5" --force --abi "armeabi-v7a"
 /usr/local/android-sdk/emulator/emulator -avd travis -no-audio -no-window &
 adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done; input keyevent 82'
