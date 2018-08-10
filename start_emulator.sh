72457993747488ab1a2be5d3af0dc85b48acc05d
Gabriel Peal <gabriel.peal@airbnb.com>
2018-08-09 21:18:44 -0700

    Fixed path

--- start_emulator.sh
+++ start_emulator.sh
@@ -1,3 +1,3 @@
 echo no | ./avdmanager create avd -n travis -k "system-images;android-27;default;x86_64" --device "Nexus 5" --force
-$ANDROID_HOME/tools/bin/emulator -avd travis -no-audio -no-window &
-$ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done; input keyevent 82'
+$ANDROID_HOMEtools/bin/emulator -avd travis -no-audio -no-window &
+$ANDROID_HOMEplatform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done; input keyevent 82'
