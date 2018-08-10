b2e06b43da2b40371788bcce4033d5eeb66ffb91
Gabriel Peal <gabriel.peal@airbnb.com>
2018-08-09 23:44:15 -0700

    Fix paths

--- start_emulator.sh
+++ start_emulator.sh
@@ -3,6 +3,6 @@ set -e
 set -o pipefail
 
 # yes | /usr/local/android-sdk/tools/bin/sdkmanager "system-images;android-24;default;armeabi-v7a"
-echo no | avdmanager create avd -n travis -k "system-images;android-24;default;armeabi-v7a" --device "Nexus 5" --force --abi "armeabi-v7a"
+echo no | /usr/local/android-sdk/tools/bin/avdmanager create avd -n travis -k "system-images;android-24;default;armeabi-v7a" --device "Nexus 5" --force --abi "armeabi-v7a"
 /usr/local/android-sdk/emulator/emulator -avd travis -no-audio -no-window &
-adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done; input keyevent 82'
+/usr/local/android-sdk/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done; input keyevent 82'
