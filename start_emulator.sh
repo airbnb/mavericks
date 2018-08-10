ee5c5806c1cc4274c9b7d094f0b076790502c7a1
Gabriel Peal <gabriel.peal@airbnb.com>
2018-08-09 23:02:25 -0700

    Force abi

--- start_emulator.sh
+++ start_emulator.sh
@@ -2,6 +2,6 @@
 set -e
 set -o pipefail
 
-echo no | avdmanager create avd -n travis -k "system-images;android-24;default;armeabi-v7a" --device "Nexus 5" --force
+echo no | avdmanager create avd -n travis -k "system-images;android-24;default;armeabi-v7a" --device "Nexus 5" --force --abi "armeabi-v7a"
 $ANDROID_HOME/emulator/emulator -avd travis -no-audio -no-window &
 adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done; input keyevent 82'
