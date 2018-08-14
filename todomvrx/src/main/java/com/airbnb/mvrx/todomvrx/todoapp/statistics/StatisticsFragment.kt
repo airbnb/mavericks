6f04e4fdb57821ad48e2b7cc206e0b2afa3ca474
Gabriel Peal <gabriel.peal@airbnb.com>
2018-08-14 13:41:38 -0700

    todo-mvp-kotlin builds with most of the code commented out

---
+++ todomvrx/src/main/java/com/airbnb/mvrx/todomvrx/todoapp/statistics/StatisticsFragment.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2017, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.airbnb.mvrx.todomvrx.todoapp.statistics
+
+import android.support.v4.app.Fragment
+
+/**
+ * Main UI for the statistics screen.
+ */
+class StatisticsFragment : Fragment() {
+
+//    private lateinit var viewDataBinding: StatisticsFragBinding
+//
+//    private lateinit var statisticsViewModel: StatisticsViewModel
+//
+//    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+//            savedInstanceState: Bundle?): View? {
+//        viewDataBinding = DataBindingUtil.inflate(inflater, R.layout.statistics_frag, container,
+//                false)
+//        return viewDataBinding.root
+//    }
+//
+//    override fun onActivityCreated(savedInstanceState: Bundle?) {
+//        super.onActivityCreated(savedInstanceState)
+//        statisticsViewModel = (activity as StatisticsActivity).obtainViewModel()
+//        viewDataBinding.stats = statisticsViewModel
+//    }
+//
+//    override fun onResume() {
+//        super.onResume()
+//        statisticsViewModel.start()
+//    }
+
+    companion object {
+        fun newInstance() = StatisticsFragment()
+    }
+}
