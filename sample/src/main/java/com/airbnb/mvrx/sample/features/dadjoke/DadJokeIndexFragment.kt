3b230dc7ca4c5fb6446a0bec911fb5da484f71dc
Gabriel Peal <gabriel@gpeal.com>
2018-08-22 13:45:55 -0700

    Added RandomDadJokeFragment

--- sample/src/main/java/com/airbnb/mvrx/sample/features/dadjoke/DadJokeIndexFragment.kt
+++ sample/src/main/java/com/airbnb/mvrx/sample/features/dadjoke/DadJokeIndexFragment.kt
@@ -2,74 +2,18 @@ package com.airbnb.mvrx.sample.features.dadjoke
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.v4.app.FragmentActivity
 import android.util.Log
 import android.view.View
 import com.airbnb.epoxy.EpoxyController
-import com.airbnb.mvrx.Async
-import com.airbnb.mvrx.BaseMvRxViewModel
 import com.airbnb.mvrx.Fail
-import com.airbnb.mvrx.Loading
-import com.airbnb.mvrx.MvRxState
-import com.airbnb.mvrx.MvRxViewModelFactory
-import com.airbnb.mvrx.Uninitialized
 import com.airbnb.mvrx.fragmentViewModel
 import com.airbnb.mvrx.onFail
 import com.airbnb.mvrx.sample.R
 import com.airbnb.mvrx.sample.core.BaseFragment
-import com.airbnb.mvrx.sample.core.MvRxViewModel
-import com.airbnb.mvrx.sample.models.Joke
-import com.airbnb.mvrx.sample.models.JokesResponse
-import com.airbnb.mvrx.sample.network.DadJokeService
 import com.airbnb.mvrx.sample.views.basicRow
 import com.airbnb.mvrx.sample.views.loadingRow
 import com.airbnb.mvrx.sample.views.marquee
 import com.airbnb.mvrx.withState
-import org.koin.android.ext.android.inject
-
-data class DadJokeIndexState(
-        /** We use this request to store the list of all jokes */
-        val jokes: List<Joke> = emptyList(),
-        /** We use this Async to keep track of the state of the current network request */
-        val request: Async<JokesResponse> = Uninitialized
-) : MvRxState
-
-private const val JOKES_PER_PAGE = 5
-
-/**
- * initialState *must* be implemented as a constructor parameter.
- */
-class DadJokeIndexViewModel(
-        initialState: DadJokeIndexState,
-        private val dadJokeService: DadJokeService
-) : MvRxViewModel<DadJokeIndexState>(initialState) {
-
-    init {
-        fetchNextPage()
-    }
-
-    fun fetchNextPage() = withState { state ->
-        if (state.request is Loading) return@withState
-
-        dadJokeService
-                .search(page = state.jokes.size / JOKES_PER_PAGE + 1, limit = JOKES_PER_PAGE)
-                .execute { copy(request = it, jokes = jokes + (it()?.results ?: emptyList())) }
-
-    }
-
-    /**
-     * If you implement MvRxViewModelFactory in your companion object, MvRx will use that to create
-     * your ViewModel. You can use this to achieve constructor dependency injection with MvRx.
-     *
-     * @see MvRxViewModelFactory
-     */
-    companion object : MvRxViewModelFactory<DadJokeIndexState> {
-        override fun create(activity: FragmentActivity, state: DadJokeIndexState): BaseMvRxViewModel<DadJokeIndexState> {
-            val service: DadJokeService by activity.inject()
-            return DadJokeIndexViewModel(state, service)
-        }
-    }
-}
 
 private const val TAG = "DadJokeIndexFragment"
 class DadJokeIndexFragment : BaseFragment() {
---
+++ sample/src/main/java/com/airbnb/mvrx/sample/features/dadjoke/DadJokeIndexViewModel.kt
@@ -0,0 +1,53 @@
+package com.airbnb.mvrx.sample.features.dadjoke
+
+import android.support.v4.app.FragmentActivity
+import com.airbnb.mvrx.*
+import com.airbnb.mvrx.sample.core.MvRxViewModel
+import com.airbnb.mvrx.sample.models.Joke
+import com.airbnb.mvrx.sample.models.JokesResponse
+import com.airbnb.mvrx.sample.network.DadJokeService
+import org.koin.android.ext.android.inject
+
+private const val JOKES_PER_PAGE = 5
+
+data class DadJokeIndexState(
+        /** We use this request to store the list of all jokes */
+        val jokes: List<Joke> = emptyList(),
+        /** We use this Async to keep track of the state of the current network request */
+        val request: Async<JokesResponse> = Uninitialized
+) : MvRxState
+
+/**
+ * initialState *must* be implemented as a constructor parameter.
+ */
+class DadJokeIndexViewModel(
+        initialState: DadJokeIndexState,
+        private val dadJokeService: DadJokeService
+) : MvRxViewModel<DadJokeIndexState>(initialState) {
+
+    init {
+        fetchNextPage()
+    }
+
+    fun fetchNextPage() = withState { state ->
+        if (state.request is Loading) return@withState
+
+        dadJokeService
+                .search(page = state.jokes.size / JOKES_PER_PAGE + 1, limit = JOKES_PER_PAGE)
+                .execute { copy(request = it, jokes = jokes + (it()?.results ?: emptyList())) }
+
+    }
+
+    /**
+     * If you implement MvRxViewModelFactory in your companion object, MvRx will use that to create
+     * your ViewModel. You can use this to achieve constructor dependency injection with MvRx.
+     *
+     * @see MvRxViewModelFactory
+     */
+    companion object : MvRxViewModelFactory<DadJokeIndexState> {
+        override fun create(activity: FragmentActivity, state: DadJokeIndexState): BaseMvRxViewModel<DadJokeIndexState> {
+            val service: DadJokeService by activity.inject()
+            return DadJokeIndexViewModel(state, service)
+        }
+    }
+}
