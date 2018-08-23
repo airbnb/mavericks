1be9e55089cfd08adb2a0e738302c4f74b5011fb
Gabriel Peal <gpeal@users.noreply.github.com>
2018-08-22 18:26:46 -0700

    Fixed selectSubscribe (#39)
    There were some shitty copy and paste bugs

--- mvrx/src/test/kotlin/com/airbnb/mvrx/ViewModelSubscriberTest.kt
+++ mvrx/src/test/kotlin/com/airbnb/mvrx/ViewModelSubscriberTest.kt
@@ -1,27 +1,37 @@
 package com.airbnb.mvrx
 
+import android.arch.lifecycle.Lifecycle
 import io.reactivex.Maybe
 import io.reactivex.disposables.Disposable
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNull
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
 
-data class ViewModelTestState(val foo: Int = 0) : MvRxState
+data class ViewModelTestState(val foo: Int = 0, val bar: Int = 0, val bam: Int = 0) : MvRxState
 class ViewModelTestViewModel(initialState: ViewModelTestState) : TestMvRxViewModel<ViewModelTestState>(initialState) {
 
     var subscribeCallCount = 0
     var subscribeWithHistoryCallCount = 0
-    var selectSubscribeCalled = 0
+    var selectSubscribe1Called = 0
+    var selectSubscribe2Called = 0
+    var selectSubscribe3Called = 0
 
     init {
         subscribe { _ -> subscribeCallCount++ }
         subscribeWithHistory { _, _ ->  subscribeWithHistoryCallCount++ }
-        selectSubscribe(ViewModelTestState::foo) { selectSubscribeCalled++ }
+        selectSubscribe(ViewModelTestState::foo) { selectSubscribe1Called++ }
+        selectSubscribe(ViewModelTestState::foo, ViewModelTestState::bar) { _, _ -> selectSubscribe2Called++ }
+        selectSubscribe(ViewModelTestState::foo, ViewModelTestState::bar, ViewModelTestState::bam) { _, _, _ -> selectSubscribe3Called++ }
     }
 
     fun setFoo(foo: Int) = setState { copy(foo = foo) }
+
+    fun setBar(bar: Int) = setState { copy(bar = bar) }
+
+    fun setBam(bam: Int) = setState { copy(bam = bam) }
 
     fun disposeOnClear(disposable: Disposable) {
         disposable.disposeOnClear()
@@ -32,13 +42,16 @@ class ViewModelTestViewModel(initialState: ViewModelTestState) : TestMvRxViewMod
     }
 }
 
-class ViewModelTest : BaseTest() {
+class ViewModelSubscriberTest : BaseTest() {
 
     private lateinit var viewModel: ViewModelTestViewModel
+    private lateinit var owner: TestLifecycleOwner
 
     @Before
     fun setup() {
         viewModel = ViewModelTestViewModel(ViewModelTestState())
+        owner = TestLifecycleOwner()
+        owner.lifecycle.markState(Lifecycle.State.RESUMED)
     }
 
     @Test
@@ -47,27 +60,77 @@ class ViewModelTest : BaseTest() {
     }
 
     @Test
+    fun testSubscribeWithHistoryExternal() {
+        var callCount = 0
+        viewModel.subscribeWithHistory(owner) { _, _ ->
+            callCount++
+        }
+        assertEquals(1, callCount)
+    }
+
+    @Test
     fun testSubscribe() {
         assertEquals(1, viewModel.subscribeCallCount)
     }
 
     @Test
-    fun testSelectSubscribe() {
-        assertEquals(0, viewModel.selectSubscribeCalled)
+    fun testSubscribeExternal() {
+        var callCount = 0
+        viewModel.subscribe(owner) { callCount++ }
+        assertEquals(1, callCount)
     }
 
     @Test
-    fun testChangeState() {
-        assertEquals(1, viewModel.subscribeCallCount)
-        assertEquals(0, viewModel.selectSubscribeCalled)
-        assertEquals(1, viewModel.subscribeWithHistoryCallCount)
+    fun testSelectSubscribe() {
+        assertEquals(0, viewModel.selectSubscribe1Called)
+    }
+
+    @Test
+    fun testSelectSubscribe1External() {
+        var callCount = 0
+        viewModel.selectSubscribe(owner, ViewModelTestState::foo) { callCount++ }
+        assertEquals(0, callCount)
+        viewModel.setFoo(1)
+        assertEquals(1, callCount)
+    }
+
+    @Test
+    fun testNotChangingFoo() {
         viewModel.setFoo(0)
         assertEquals(1, viewModel.subscribeCallCount)
-        assertEquals(0, viewModel.selectSubscribeCalled)
+        assertEquals(0, viewModel.selectSubscribe1Called)
+        assertEquals(0, viewModel.selectSubscribe2Called)
+        assertEquals(0, viewModel.selectSubscribe3Called)
         assertEquals(1, viewModel.subscribeWithHistoryCallCount)
+    }
+
+    @Test
+    fun testChangingFoo() {
         viewModel.setFoo(1)
         assertEquals(2, viewModel.subscribeCallCount)
-        assertEquals(1, viewModel.selectSubscribeCalled)
+        assertEquals(1, viewModel.selectSubscribe1Called)
+        assertEquals(1, viewModel.selectSubscribe2Called)
+        assertEquals(1, viewModel.selectSubscribe3Called)
+        assertEquals(2, viewModel.subscribeWithHistoryCallCount)
+    }
+
+    @Test
+    fun testChangingBar() {
+        viewModel.setBar(1)
+        assertEquals(2, viewModel.subscribeCallCount)
+        assertEquals(0, viewModel.selectSubscribe1Called)
+        assertEquals(1, viewModel.selectSubscribe2Called)
+        assertEquals(1, viewModel.selectSubscribe3Called)
+        assertEquals(2, viewModel.subscribeWithHistoryCallCount)
+    }
+
+    @Test
+    fun testChangingBam() {
+        viewModel.setBam(1)
+        assertEquals(2, viewModel.subscribeCallCount)
+        assertEquals(0, viewModel.selectSubscribe1Called)
+        assertEquals(0, viewModel.selectSubscribe2Called)
+        assertEquals(1, viewModel.selectSubscribe3Called)
         assertEquals(2, viewModel.subscribeWithHistoryCallCount)
     }
 
