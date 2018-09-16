845d821c718958069cbcd00fe143e510a0a692b2
Roman Zavarnitsyn <rom4ek93@gmail.com>
2018-09-17 00:00:13 +0200

    * Fix formatting * Use correct BuildConifg

--- todomvrx/src/main/java/com/airbnb/mvrx/todomvrx/core/MvRxViewModel.kt
+++ todomvrx/src/main/java/com/airbnb/mvrx/todomvrx/core/MvRxViewModel.kt
@@ -1,7 +1,7 @@
 package com.airbnb.mvrx.todomvrx.core
 
 import com.airbnb.mvrx.BaseMvRxViewModel
-import com.airbnb.mvrx.BuildConfig
 import com.airbnb.mvrx.MvRxState
+import com.airbnb.mvrx.todomvrx.todoapp.BuildConfig
 
 abstract class MvRxViewModel<S : MvRxState>(initialState: S) : BaseMvRxViewModel<S>(initialState, debugMode = BuildConfig.DEBUG)
