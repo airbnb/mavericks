4c4b6f67c78cc503f55e7c67c87ec2292a6dcb87
Huan Lin <huan.lin@airbnb.com>
2019-01-02 16:13:22 -0800

    use subscribeOn(io) instead of delay

--- sample/src/main/java/com/airbnb/mvrx/sample/features/dadjoke/DadJokeIndexViewModel.kt
+++ sample/src/main/java/com/airbnb/mvrx/sample/features/dadjoke/DadJokeIndexViewModel.kt
@@ -11,8 +11,8 @@ import com.airbnb.mvrx.sample.core.MvRxViewModel
 import com.airbnb.mvrx.sample.models.Joke
 import com.airbnb.mvrx.sample.models.JokesResponse
 import com.airbnb.mvrx.sample.network.DadJokeService
+import io.reactivex.schedulers.Schedulers
 import org.koin.android.ext.android.inject
-import java.util.concurrent.TimeUnit
 
 private const val JOKES_PER_PAGE = 5
 
@@ -40,8 +40,7 @@ class DadJokeIndexViewModel(
 
         dadJokeService
                 .search(page = state.jokes.size / JOKES_PER_PAGE + 1, limit = JOKES_PER_PAGE)
-                // Use delay to mimic async behavior
-                .delay(1, TimeUnit.MILLISECONDS)
+                .subscribeOn(Schedulers.io())
                 .execute { copy(request = it, jokes = jokes + (it()?.results ?: emptyList())) }
     }
 
