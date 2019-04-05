5e7c541202a3202adf3e618c00d8e700a52e78a8
Gabriel Peal <gabriel@tonal.com>
2019-04-05 11:28:07 +0200

    Readded some commented code

--- dogs/src/main/java/com/airbnb/mvrx/dogs/AdoptionFragment.kt
+++ dogs/src/main/java/com/airbnb/mvrx/dogs/AdoptionFragment.kt
@@ -4,10 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import com.airbnb.mvrx.BaseMvRxFragment
 
-class AdoptionFragment : Fragment() {
+class AdoptionFragment : BaseMvRxFragment() {
+//    private val viewModel: DogsViewModel by activityViewModel()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_adoption, container, false)
     }
+
+    override fun invalidate() {
+        
+    }
+
+//    override fun invalidate() = withState(viewModel) { state ->
+//        dogsRecyclerView.withModels {
+//            titleRow {
+//                id("title")
+//                title(R.string.meet_your_dog)
+//            }
+//            state.adoptionRequest()?.let { dog ->
+//                dogRow {
+//                    id(dog.id)
+//                    dog(dog)
+//                }
+//            }
+//        }
+//    }
 }
--- dogs/src/main/java/com/airbnb/mvrx/dogs/DogDetailFragment.kt
+++ dogs/src/main/java/com/airbnb/mvrx/dogs/DogDetailFragment.kt
@@ -4,10 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.airbnb.mvrx.BaseMvRxFragment
+import kotlinx.android.synthetic.main.fragment_dogs.adoptButton
 
-class DogDetailFragment : Fragment() {
+class DogDetailFragment : BaseMvRxFragment() {
+
+//    private val viewModel: DogsViewModel by activityViewModel()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_dog_detail, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        adoptButton.setOnClickListener {
+            //            viewModel.loveDog(dogId)
+            findNavController().popBackStack()
+        }
+    }
+
+    override fun invalidate() {
+
+    }
+
+//    override fun invalidate() = withState(viewModel) { state ->
+//        val dog = state.dog(dogId)
+//        Picasso.with(requireContext())
+//            .load(dog.imageUrl)
+//            .into(image)
+//        nameView.text = dog.name
+//        breedsView.text = dog.breeds
+//        descriptionView.text = dog.description
+//    }
 }
