1b1658be38f546ba90acab76ea8bc380fa19e610
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-10-13 13:31:46 +0530

    fix: Simplify rendering logic in HelloFragment

--- hellodagger/src/main/java/com/airbnb/mvrx/helloDagger/HelloFragment.kt
+++ hellodagger/src/main/java/com/airbnb/mvrx/helloDagger/HelloFragment.kt
@@ -20,22 +20,12 @@ class HelloFragment: BaseMvRxFragment() {
         helloButton.setOnClickListener { viewModel.sayHello() }
     }
 
-    override fun invalidate() {
-        withState(viewModel) { state: HelloState ->
-            when (state.message) {
-                is Uninitialized, is Loading -> {
-                    messageTextView.text = getString(R.string.hello_fragment_loading_text)
-                    helloButton.isEnabled = false
-                }
-                is Success -> {
-                    messageTextView.text = state.message()
-                    helloButton.isEnabled = true
-                }
-                is Fail -> {
-                    messageTextView.text = getString(R.string.hello_fragment_failure_text)
-                    helloButton.isEnabled = true
-                }
-            }
+    override fun invalidate() = withState(viewModel) { state ->
+        helloButton.isEnabled = state.message !is Loading
+        messageTextView.text = when (state.message) {
+            is Uninitialized, is Loading ->  getString(R.string.hello_fragment_loading_text)
+            is Success -> state.message()
+            is Fail ->  getString(R.string.hello_fragment_failure_text)
         }
     }
 }
