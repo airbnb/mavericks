c1e278e3354fe8dc8d70b23a38332c7411e57276
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-10-14 23:33:24 +0530

    fix: Cleanup HelloFragment
    Removes the call to 'super.onViewCreated()' and moves layout resource ID to the constructor.

--- hellodagger/src/main/java/com/airbnb/mvrx/helloDagger/HelloFragment.kt
+++ hellodagger/src/main/java/com/airbnb/mvrx/helloDagger/HelloFragment.kt
@@ -1,22 +1,15 @@
 package com.airbnb.mvrx.helloDagger
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.airbnb.mvrx.*
 import kotlinx.android.synthetic.main.fragment_hello.*
 
-class HelloFragment: BaseMvRxFragment() {
+class HelloFragment: BaseMvRxFragment(R.layout.fragment_hello) {
 
     private val viewModel: HelloViewModel by fragmentViewModel()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_hello, container, false)
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
         helloButton.setOnClickListener { viewModel.sayHello() }
     }
 
