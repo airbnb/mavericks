c49e7558db7ddba3fdee2de83ddab2d042e21821
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-10-15 12:25:45 +0530

    fix: Fix never ending HelloFragmentTest error

--- hellodagger/src/androidTest/java/com/airbnb/mvrx/helloDagger/HelloFragmentTest.kt
+++ hellodagger/src/androidTest/java/com/airbnb/mvrx/helloDagger/HelloFragmentTest.kt
@@ -17,15 +17,13 @@ class HelloFragmentTest {
 
         scenario.onActivity { activity: MainActivity ->
             val fragment = activity.findFragmentById<HelloFragment>(R.id.rootContainer)
-
             withState(fragment.viewModel) {
                 assert(it.message is Loading)
             }
-
-            onView(withId(R.id.messageTextView)).check(matches(withText(R.string.hello_fragment_loading_text)))
-            onView(withId(R.id.helloButton)).check(matches(not(isEnabled())))
         }
-    }
 
+        onView(withId(R.id.messageTextView)).check(matches(withText(R.string.hello_fragment_loading_text)))
+        onView(withId(R.id.helloButton)).check(matches(not(isEnabled())))
+    }
 }
 
