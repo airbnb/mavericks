e255b94b117d5574fd4d8896434786243dab015a
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-10-15 10:52:19 +0530

    refactor: Use FragmentContainerView and constructor layout res ID in Activities

--- hellodagger/src/main/java/com/airbnb/mvrx/helloDagger/MainActivity.kt
+++ hellodagger/src/main/java/com/airbnb/mvrx/helloDagger/MainActivity.kt
@@ -1,17 +1,5 @@
 package com.airbnb.mvrx.helloDagger
 
-import android.os.Bundle
 import com.airbnb.mvrx.helloDagger.base.MvRxActivity
 
-class MainActivity : MvRxActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.rootContainer, HelloFragment())
-                    .commit()
-        }
-    }
-}
+class MainActivity : MvRxActivity(R.layout.activity_main)
--- hellodagger/src/main/java/com/airbnb/mvrx/helloDagger/base/MvRxActivity.kt
+++ hellodagger/src/main/java/com/airbnb/mvrx/helloDagger/base/MvRxActivity.kt
@@ -1,6 +1,7 @@
 package com.airbnb.mvrx.helloDagger.base
 
 import android.os.Bundle
+import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import com.airbnb.mvrx.helloDagger.appComponent
 import com.airbnb.mvrx.helloDagger.di.AssistedViewModelFactory
@@ -12,7 +13,7 @@ import javax.inject.Inject
  * This class contains an injected map of [AssistedViewModelFactory]s to make it easier to create
  * and instantiate requested ViewModels.
  */
-abstract class MvRxActivity : AppCompatActivity() {
+abstract class MvRxActivity(@LayoutRes layoutResId: Int = 0) : AppCompatActivity(layoutResId) {
 
     @Inject
     lateinit var viewModelFactoryMap: @JvmSuppressWildcards Map<Class<out MvRxViewModel<*>>, AssistedViewModelFactory<*, *>>
