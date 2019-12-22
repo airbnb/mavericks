656e15f65bb01fe3609210ed343be08dc954a36f
Gabriel Peal <gabriel@tonal.com>
2019-12-21 21:31:05 -0800

    Add improve data class check

--- mvrx/src/main/kotlin/com/airbnb/mvrx/MvRxMutabilityHelper.kt
+++ mvrx/src/main/kotlin/com/airbnb/mvrx/MvRxMutabilityHelper.kt
@@ -64,9 +64,9 @@ private val Class<*>.isData: Boolean
         // A data class needs at least one field and it will automatically have a component1 method generated.
         // These componentN methods are also used for @PersistState.
         declaredMethods.firstOrNull { it.name == "component1" } ?: return false
-        @Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
-        val annotationData = getDeclaredAnnotation(Metadata::class.java).data2
-        return annotationData.contains("hashCode") && annotationData.contains("equals")
+        declaredMethods.firstOrNull { it.name == "equals" } ?: return false
+        declaredMethods.firstOrNull { it.name == "hashCode" } ?: return false
+        return true
     }
 
 /**
