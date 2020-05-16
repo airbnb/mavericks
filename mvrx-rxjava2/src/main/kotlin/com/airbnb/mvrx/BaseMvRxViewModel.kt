426edd28c18c76421213118a41943ad9f5f939aa
Gabriel Peal <gabriel@tonal.com>
2020-05-16 12:11:51 -0700

    Deleted some classes from rxjava2

--- mvrx-rxjava2/src/main/kotlin/com/airbnb/mvrx/BaseMvRxViewModel.kt
+++ mvrx-rxjava2/src/main/kotlin/com/airbnb/mvrx/BaseMvRxViewModel.kt
@@ -786,37 +786,3 @@ abstract class BaseMvRxViewModel<S : MvRxState>(
 
     override fun toString(): String = "${this::class.java.simpleName} $state"
 }
-
-/**
- * Defines what updates a subscription should receive.
- * See: [RedeliverOnStart], [UniqueOnly].
- */
-sealed class DeliveryMode {
-
-    internal fun appendPropertiesToId(vararg properties: KProperty1<*, *>): DeliveryMode {
-        return when (this) {
-            is RedeliverOnStart -> RedeliverOnStart
-            is UniqueOnly -> UniqueOnly(subscriptionId + "_" + properties.joinToString(",") { it.name })
-        }
-    }
-}
-
-/**
- * The subscription will receive the most recent state update when transitioning from locked to unlocked states (stopped -> started),
- * even if the state has not changed while locked.
- *
- * Likewise, when a MvRxView resubscribes after a configuration change the most recent update will always be emitted.
- */
-object RedeliverOnStart : DeliveryMode()
-
-/**
- * The subscription will receive the most recent state update when transitioning from locked to unlocked states (stopped -> started),
- * only if the state has changed while locked.
- *
- * Likewise, when a MvRxView resubscribes after a configuration change the most recent update will only be emitted
- * if the state has changed while locked.
- *
- * @param subscriptionId A uniqueIdentifier for this subscription. It is an error for two unique only subscriptions to
- * have the same id.
- */
-class UniqueOnly(val subscriptionId: String) : DeliveryMode()
