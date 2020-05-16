d6d29abe6f9b643341116cf3d93ff7865c523049
Gabriel Peal <gabriel@tonal.com>
2020-05-16 13:42:10 -0700

    Move tuples

--- mvrx/src/main/kotlin/com/airbnb/mvrx/MvRxTuples.kt
+++
@@ -1,9 +0,0 @@
-package com.airbnb.mvrx
-
-data class MvRxTuple1<A>(val a: A)
-data class MvRxTuple2<A, B>(val a: A, val b: B)
-data class MvRxTuple3<A, B, C>(val a: A, val b: B, val c: C)
-data class MvRxTuple4<A, B, C, D>(val a: A, val b: B, val c: C, val d: D)
-data class MvRxTuple5<A, B, C, D, E>(val a: A, val b: B, val c: C, val d: D, val e: E)
-data class MvRxTuple6<A, B, C, D, E, F>(val a: A, val b: B, val c: C, val d: D, val e: E, val f: F)
-data class MvRxTuple7<A, B, C, D, E, F, G>(val a: A, val b: B, val c: C, val d: D, val e: E, val f: F, val g: G)
