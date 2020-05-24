570cc4a0ba79f1aef8bd19533579200aac115849
Gabriel Peal <gabriel@tonal.com>
2020-05-23 17:09:09 -0700

    Cleaned up sample app

--- sample/src/main/java/com/airbnb/mvrx/sample/models/JokesResponse.kt
+++ sample/src/main/java/com/airbnb/mvrx/sample/models/JokesResponse.kt
@@ -3,6 +3,6 @@ package com.airbnb.mvrx.sample.models
 import com.squareup.moshi.Json
 
 data class JokesResponse(
-    @get:Json(name = "next_page") @Json(name = "next_page") val nextPage: Int,
-    @get:Json(name = "results") @Json(name = "results") val results: List<Joke>
+    @Json(name = "next_page") val nextPage: Int,
+    val results: List<Joke>
 )
