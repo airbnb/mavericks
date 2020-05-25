3b8c7b888208ec7ccd0e4bc36b12b979263cfea1
Gabriel Peal <gabriel@tonal.com>
2020-05-25 16:40:11 -0700

    Added test

---
+++ mvrx/src/test/kotlin/com/airbnb/mvrx/MvRxExtensionsKtTest.kt
@@ -0,0 +1,21 @@
+package com.airbnb.mvrx
+
+import android.graphics.Rect
+import org.junit.Assert.*
+import org.junit.Test
+
+class MvRxExtensionsKtTest : BaseTest() {
+    @Test
+    fun testParcelableIntoArgs() {
+        val rect = Rect(1, 2, 3, 4)
+        val args = rect.asMavericksArgs()
+        assertEquals(rect, args.getParcelable<Rect>(MvRx.KEY_ARG))
+    }
+
+    @Test
+    fun testSerializableIntoArgs() {
+        val value = true
+        val args = value.asMavericksArgs()
+        assertEquals(true, args.getSerializable(MvRx.KEY_ARG))
+    }
+}
