ab809b49da3351a80a8f14872878458b588b8435
Carlos M <carlos@turo.com>
2020-06-22 21:18:54 -0700

    Adds DisableInstallInCheck explanation.

--- hellohilt/src/main/java/com/airbnb/mvrx/hellohilt/di/AppModule.kt
+++ hellohilt/src/main/java/com/airbnb/mvrx/hellohilt/di/AppModule.kt
@@ -9,6 +9,12 @@ import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.migration.DisableInstallInCheck
 import dagger.multibindings.IntoMap
 
+/**
+ * Hilt requires included modules to be annotated with [InstallIn], as issues due to forgetting are potentially difficult to track.
+ *
+ * However, since the included [AssistedInject_AppModule] is auto-generated, for now we need to disable this check. Hilt allows us
+ * to do it via [DisableInstallInCheck], along with the `disableModulesHaveInstallInCheck` compiler option declared in the module's build.gradle.
+ */
 @AssistedModule
 @Module(includes = [AssistedInject_AppModule::class])
 @DisableInstallInCheck
