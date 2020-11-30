0599609256872c0a6a39f8fe30150547b05cef7f
Gabriel Peal <gabriel@tonal.com>
2020-11-30 12:56:29 -0800

    use postDelayed extension

--- launcher/src/main/java/com/airbnb/mvrx/launcher/MavericksLauncherMockActivity.kt
+++ launcher/src/main/java/com/airbnb/mvrx/launcher/MavericksLauncherMockActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Parcelable
 import android.util.Log
+import androidx.core.os.postDelayed
 import androidx.fragment.app.Fragment
 import com.airbnb.mvrx.MavericksView
 import com.airbnb.mvrx.MavericksViewModelConfig
@@ -158,10 +159,10 @@ class MavericksLauncherMockActivity : MavericksBaseLauncherActivity() {
                         mock.mock.isDefaultInitialization || mock.mock.isForProcessRecreation
                     val finishAfterMs: Long = if (isInitializing) 3000 else 500
 
-                    Handler().postDelayed({
-                      onMockLoaded(activity, mock, mockedView)
-                      activity.finish()
-                  }, finishAfterMs)
+                    Handler().postDelayed(finishAfterMs) {
+                        onMockLoaded(activity, mock, mockedView)
+                        activity.finish()
+                    }
                 }
             } catch (e: Throwable) {
                 Log.e(
