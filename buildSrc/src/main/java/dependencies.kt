58e47e56d5be56d32792f88def5f11ebae780b98
Gabriel Peal <gabriel@tonal.com>
2021-02-28 17:12:36 -0800

    Upgrade to 30

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -1,7 +1,7 @@
 object Versions {
     // Build tools and SDK
-    const val buildTools = "29.0.3"
-    const val compileSdk = 29
+    const val buildTools = "30.0.2"
+    const val compileSdk = 30
     const val gradlePlugin = "7.0.0-alpha08"
     const val kotlin = "1.4.30"
     const val minSdk = 16
