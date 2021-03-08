6349e970f12ff0edc56ac29383b5e5acc6d086fc
Mitchell Tilbrook <mitchell.tilbrook@safetyculture.io>
2021-03-09 10:02:38 +1100

    Add MockableMavericks to Dagger example

--- hellodagger/src/main/java/com/airbnb/mvrx/hellodagger/HelloFragment.kt
+++ hellodagger/src/main/java/com/airbnb/mvrx/hellodagger/HelloFragment.kt
@@ -1,6 +1,7 @@
 package com.airbnb.mvrx.hellodagger
 
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.airbnb.mvrx.Fail
@@ -9,11 +10,14 @@ import com.airbnb.mvrx.MavericksView
 import com.airbnb.mvrx.Success
 import com.airbnb.mvrx.Uninitialized
 import com.airbnb.mvrx.fragmentViewModel
+import com.airbnb.mvrx.mocking.MavericksViewMocks
+import com.airbnb.mvrx.mocking.MockableMavericksView
+import com.airbnb.mvrx.mocking.mockSingleViewModel
 import com.airbnb.mvrx.withState
 import kotlinx.android.synthetic.main.fragment_hello.helloButton
 import kotlinx.android.synthetic.main.fragment_hello.messageTextView
 
-class HelloFragment : Fragment(R.layout.fragment_hello), MavericksView {
+class HelloFragment : Fragment(R.layout.fragment_hello), MockableMavericksView {
 
     val viewModel: HelloDaggerViewModel by fragmentViewModel()
 
@@ -27,6 +31,16 @@ class HelloFragment : Fragment(R.layout.fragment_hello), MavericksView {
             is Uninitialized, is Loading -> getString(R.string.hello_fragment_loading_text)
             is Success -> state.message()
             is Fail -> getString(R.string.hello_fragment_failure_text)
+        }
+    }
+
+    override fun provideMocks(): MavericksViewMocks<out MockableMavericksView, out Parcelable> = mockSingleViewModel(
+        viewModelReference = HelloFragment::viewModel,
+        defaultState = HelloDaggerState(),
+        defaultArgs = null
+    ) {
+        state("successfully") {
+            copy(message = Success("I haz loaded"))
         }
     }
 }
