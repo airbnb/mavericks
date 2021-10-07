d6b9408eea7889f2314cefb2cbdf787ee64f5776
Jakub Syty <jakub.nekro@gmail.com>
2021-10-07 15:07:56 +0200

    Fix issue when the LocalContext is not directly an Activity

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -3,7 +3,7 @@ object Versions {
     const val buildTools = "30.0.3"
     const val compileSdk = 30
     const val gradlePlugin = "7.0.0"
-    const val kotlin = "1.5.21"
+    const val kotlin = "1.5.30"
     const val minSdk = 16
     const val targetSdk = 29
 
@@ -15,7 +15,7 @@ object Versions {
     const val cardview = "1.0.0"
     const val constraintlayout = "2.0.0"
     const val coordinatorLayout = "1.1.0"
-    const val compose = "1.0.2"
+    const val compose = "1.0.3"
     const val core = "1.5.0"
     const val fragment = "1.3.4"
     const val lifecycle = "2.3.1"
