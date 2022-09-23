aa7ca932377e311a96fdc64b151fb165fd387467
Gabriel Peal <gabriel@tonal.com>
2022-09-23 10:02:37 -0700

    Detekt

--- mvrx-compose/src/test/kotlin/com/airbnb/mvrx/compose/ArgsFactoryTest.kt
+++ mvrx-compose/src/test/kotlin/com/airbnb/mvrx/compose/ArgsFactoryTest.kt
@@ -6,7 +6,6 @@ import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
